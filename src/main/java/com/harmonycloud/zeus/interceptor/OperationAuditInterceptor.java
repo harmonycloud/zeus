58ab0745f6fe9e8a870bf1440d848c4251f21c33
liyinlong <liyinlong>
2021-09-01 14:18:33 +0800

    fix - 操作审计无法获取真实请求ip

--- src/main/java/com/harmonycloud/zeus/interceptor/OperationAuditInterceptor.java
+++ src/main/java/com/harmonycloud/zeus/interceptor/OperationAuditInterceptor.java
@@ -199,41 +199,12 @@ public class OperationAuditInterceptor {
      * @param request
      * @return
      */
-    public static String getIPAddress(HttpServletRequest request) {
-        String ip = null;
-        //X-Forwarded-For：Squid 服务代理
+    public String getIPAddress(HttpServletRequest request) {
         String ipAddresses = request.getHeader("X-Forwarded-For");
-
-        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            //Proxy-Client-IP：apache 服务代理
-            ipAddresses = request.getHeader("Proxy-Client-IP");
+        log.info("X-Forwarded-For:{}", ipAddresses);
+        if (ipAddresses != null) {
+            return ipAddresses;
         }
-
-        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            //WL-Proxy-Client-IP：weblogic 服务代理
-            ipAddresses = request.getHeader("WL-Proxy-Client-IP");
-        }
-
-        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            //HTTP_CLIENT_IP：有些代理服务器
-            ipAddresses = request.getHeader("HTTP_CLIENT_IP");
-        }
-
-        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            //X-Real-IP：nginx服务代理
-            ipAddresses = request.getHeader("X-Real-IP");
-        }
-
-        //有些网络通过多层代理，那么获取到的ip就会有多个，一般都是通过逗号（,）分割开来，并且第一个ip为客户端的真实IP
-        if (ipAddresses != null && ipAddresses.length() != 0) {
-            ip = ipAddresses.split(",")[0];
-        }
-
-        //还是不能获取到，最后再通过request.getRemoteAddr();获取
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
-            ip = request.getRemoteAddr();
-        }
-
-        return ip;
+        return request.getRemoteAddr();
     }
 }
