aa17bfe79695e2702398a168fa3dc0f2930f3243
KarlCyan <karlcyan1997@gmail.com>
2021-09-02 20:18:27 +0800

    Feature deploy offline (#8)
    * feature: helm install type
    * feature: deploy offline

--- build/release.sh
+++ build/release.sh
@@ -1,28 +1,19 @@
 IMAGE_FILE=$1
 REPO=$2
 IMAGES=$(cat $IMAGE_FILE | grep -vE '^$|#')
-RELEASE_PATH="release/"
-
-rm -rf $RELEASE_PATH
-mkdir $RELEASE_PATH
 
 ALL_NAME=""
 
 # image
-mkdir $RELEASE_PATH"/images"
 for IMG in $IMAGES
 do
 {
   echo "Docker pulling "$REPO"/"$IMG
   docker pull $REPO"/"$IMG
+  docker tag $REPO"/"$IMG $IMG
   NAME=$(echo $IMG | sed 's/:/-/')
-  ALL_NAME=$(echo $ALL_NAME" "$REPO"/"$IMG)
+  ALL_NAME=$(echo $ALL_NAME" "$IMG)
 }
 done
 
-docker save $ALL_NAME > $RELEASE_PATH"/images/zeus.tar"
-
-mkdir $RELEASE_PATH"/deploy"
-cp deploy/* $RELEASE_PATH"/deploy/"
-
-tar -cf release.tar $RELEASE_PATH
+docker save $ALL_NAME > "./deploy/zeus-offline.tar"
