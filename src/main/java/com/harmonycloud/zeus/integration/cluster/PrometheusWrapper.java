958a657feba2632d923d7f3f298e6fc0940ab59b
xutianhong <736950061@qq.com>
2021-09-07 15:12:04 +0800

    fix - prometheus 路径适配

--- src/main/java/com/harmonycloud/zeus/integration/cluster/PrometheusWrapper.java
+++ src/main/java/com/harmonycloud/zeus/integration/cluster/PrometheusWrapper.java
@@ -30,13 +30,16 @@ public class PrometheusWrapper {
     @Value("${k8s.monitoring.alertManager.port:31252}")
     private String alertManagerPort;
 
-    public PrometheusResponse getMonitorInfo(String clusterId, String prometheusApiVersion, Map<String, String> queryMap)
-        throws Exception {
+    public PrometheusResponse getMonitorInfo(String clusterId, String prometheusApiVersion,
+                                             Map<String, String> queryMap) throws Exception {
 
         MiddlewareClusterDTO cluster = clusterService.findById(clusterId);
         MiddlewareClusterMonitorInfo prometheus = getPrometheusInfo(cluster);
-        PrometheusApi prometheusApi = new PrometheusApi(new PrometheusClient(prometheus.getProtocol(),
-            prometheus.getHost(), Integer.parseInt(prometheus.getPort()), prometheusApiVersion));
+        PrometheusApi prometheusApi = new PrometheusApi(
+                new PrometheusClient(prometheus.getProtocol(), prometheus.getHost(), Integer.parseInt(prometheus.getPort()),
+                        prometheus.getAddress()
+                                .replace(prometheus.getProtocol() + "://" + prometheus.getHost() + ":" + prometheus.getPort(), "")
+                                + prometheusApiVersion));
         return prometheusApi.getMonitorInfo("", queryMap);
     }
 
@@ -44,28 +47,30 @@ public class PrometheusWrapper {
     public PrometheusRulesResponse getRules(String clusterId, String prometheusApiVersion) throws Exception {
         MiddlewareClusterDTO cluster = clusterService.findById(clusterId);
         MiddlewareClusterMonitorInfo prometheus = getPrometheusInfo(cluster);
-        PrometheusApi prometheusApi =
-            new PrometheusApi(new PrometheusClient(prometheus.getProtocol(),
-                    prometheus.getHost(), Integer.parseInt(prometheus.getPort()), prometheusApiVersion));
+        PrometheusApi prometheusApi = new PrometheusApi(
+                new PrometheusClient(prometheus.getProtocol(), prometheus.getHost(), Integer.parseInt(prometheus.getPort()),
+                        prometheus.getAddress()
+                                .replace(prometheus.getProtocol() + "://" + prometheus.getHost() + ":" + prometheus.getPort(), "")
+                                + prometheusApiVersion));
         return prometheusApi.getRules();
     }
 
     public void setSilence(String clusterId, String prometheusApiVersion, Map<String, Object> body) throws Exception {
         MiddlewareClusterDTO cluster = clusterService.findById(clusterId);
         MiddlewareClusterMonitorInfo alertManager = getAlertManagerInfo(cluster);
-        PrometheusApi prometheusApi =
-                new PrometheusApi(new PrometheusClient(alertManager.getProtocol(),
-                        alertManager.getHost(), Integer.parseInt(alertManager.getPort()), prometheusApiVersion));
+        PrometheusApi prometheusApi = new PrometheusApi(new PrometheusClient(alertManager.getProtocol(),
+                alertManager.getHost(), Integer.parseInt(alertManager.getPort()),
+                alertManager.getAddress()
+                        .replace(alertManager.getProtocol() + "://" + alertManager.getHost() + ":" + alertManager.getPort(), "")
+                        + prometheusApiVersion));
         prometheusApi.setSilence(body);
     }
 
     private MiddlewareClusterMonitorInfo getPrometheusInfo(MiddlewareClusterDTO cluster) {
         MiddlewareClusterMonitorInfo prometheus;
         if (cluster.getMonitor() == null || cluster.getMonitor().getPrometheus() == null) {
-            prometheus = new MiddlewareClusterMonitorInfo()
-                    .setProtocol(Protocol.HTTP.getValue().toLowerCase())
-                    .setHost(cluster.getIngress().getAddress())
-                    .setPort(prometheusPort);
+            prometheus = new MiddlewareClusterMonitorInfo().setProtocol(Protocol.HTTP.getValue().toLowerCase())
+                    .setHost(cluster.getIngress().getAddress()).setPort(prometheusPort);
         } else {
             prometheus = cluster.getMonitor().getPrometheus();
             if (StringUtils.isBlank(cluster.getMonitor().getPrometheus().getProtocol())) {
@@ -78,16 +83,17 @@ public class PrometheusWrapper {
                 prometheus.setPort(prometheusPort);
             }
         }
+        if (StringUtils.isEmpty(prometheus.getAddress())) {
+            prometheus.setAddress(prometheus.getProtocol() + "://" + prometheus.getHost() + ":" + prometheus.getPort());
+        }
         return prometheus;
     }
 
     private MiddlewareClusterMonitorInfo getAlertManagerInfo(MiddlewareClusterDTO cluster) {
         MiddlewareClusterMonitorInfo alertManager;
         if (cluster.getMonitor() == null || cluster.getMonitor().getAlertManager() == null) {
-            alertManager = new MiddlewareClusterMonitorInfo()
-                    .setProtocol(Protocol.HTTP.getValue().toLowerCase())
-                    .setHost(cluster.getIngress().getAddress())
-                    .setPort(alertManagerPort);
+            alertManager = new MiddlewareClusterMonitorInfo().setProtocol(Protocol.HTTP.getValue().toLowerCase())
+                    .setHost(cluster.getIngress().getAddress()).setPort(alertManagerPort);
         } else {
             alertManager = cluster.getMonitor().getAlertManager();
             if (StringUtils.isBlank(cluster.getMonitor().getAlertManager().getProtocol())) {
@@ -99,6 +105,10 @@ public class PrometheusWrapper {
             if (StringUtils.isBlank(cluster.getMonitor().getAlertManager().getPort())) {
                 alertManager.setPort(alertManagerPort);
             }
+        }
+        if (StringUtils.isEmpty(alertManager.getAddress())) {
+            alertManager
+                    .setAddress(alertManager.getProtocol() + "://" + alertManager.getHost() + ":" + alertManager.getPort());
         }
         return alertManager;
     }
