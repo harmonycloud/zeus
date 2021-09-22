fe94c5f8a07991832b6afbce439c45c76d5361fb
xutianhong <736950061@qq.com>
2021-09-22 15:05:29 +0800

    feat - 更新全量部署sql语句

--- deploy/docker-compose/conf/mysql/init/middleware.sql
+++ deploy/docker-compose/conf/mysql/init/middleware.sql
@@ -18,7 +18,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `alert_record`;
 CREATE TABLE `alert_record` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `cluster_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '集群id',
   `namespace` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分区',
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '中间件名称',
@@ -36,7 +36,7 @@ CREATE TABLE `alert_record` (
 -- ----------------------------
 DROP TABLE IF EXISTS `alert_rule`;
 CREATE TABLE `alert_rule` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `chart_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '类型名称',
   `chart_version` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '版本',
   `alert` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'prometheusRule内容',
@@ -44,11 +44,24 @@ CREATE TABLE `alert_rule` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='告警规则表';
 
 -- ----------------------------
+-- Table structure for cluster_middleware_info
+-- ----------------------------
+DROP TABLE IF EXISTS `cluster_middleware_info`;
+CREATE TABLE `cluster_middleware_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `cluster_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '集群id',
+  `chart_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'chart包名称',
+  `chart_version` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'chart包版本',
+  `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'operator状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='集群-中间件关联表';
+
+-- ----------------------------
 -- Table structure for custom_config
 -- ----------------------------
 DROP TABLE IF EXISTS `custom_config`;
 CREATE TABLE `custom_config` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '字段名称',
   `chart_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'chart包名称',
   `default_value` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '默认值',
@@ -65,7 +78,7 @@ CREATE TABLE `custom_config` (
 -- ----------------------------
 DROP TABLE IF EXISTS `custom_config_history`;
 CREATE TABLE `custom_config_history` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `cluster_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '集群id',
   `namespace` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '分区',
   `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '中间件名称',
@@ -83,7 +96,7 @@ CREATE TABLE `custom_config_history` (
 -- ----------------------------
 DROP TABLE IF EXISTS `custom_config_template`;
 CREATE TABLE `custom_config_template` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '模板名称',
   `alias_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '模板中文名',
   `type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '中间件类型',
@@ -92,18 +105,29 @@ CREATE TABLE `custom_config_template` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='自定义配置模板表';
 
 -- ----------------------------
+-- Table structure for k8s_default_cluster
+-- ----------------------------
+DROP TABLE IF EXISTS `k8s_default_cluster`;
+CREATE TABLE `k8s_default_cluster` (
+  `id` int(2) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `cluster_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '集群id',
+  `url` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '路径',
+  `token` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'service account',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
 -- Table structure for middleware_info
 -- ----------------------------
 DROP TABLE IF EXISTS `middleware_info`;
 CREATE TABLE `middleware_info` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '中间件名称',
   `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',
   `type` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '类型',
   `version` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '版本',
   `image` mediumblob COMMENT '图片',
   `image_path` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '图片地址',
-  `status` tinyint(1) DEFAULT '1' COMMENT '是否可用：0-不可用 1-可用',
   `chart_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'chart包名称',
   `chart_version` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'chart版本',
   `grafana_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'grafana的id',
@@ -111,8 +135,9 @@ CREATE TABLE `middleware_info` (
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `modifier` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '修改人',
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-  `cluster_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '集群id',
   `official` tinyint(1) DEFAULT NULL COMMENT '官方中间件',
+  `chart` mediumblob COMMENT 'helm chart包',
+  `operator_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'operator名称',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='中间件表';
 
@@ -158,27 +183,32 @@ CREATE TABLE `resource_menu` (
   `alias_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '中文名称',
   `url` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '路径',
   `weight` int(11) DEFAULT NULL COMMENT '权重(排序使用)',
-  `icon_name` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'icon名称',
+  `icon_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT 'icon名称',
   `parent_id` int(11) DEFAULT NULL COMMENT '父菜单id',
   `module` varchar(0) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '模块',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='菜单资源表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='菜单资源表';
 
 -- ----------------------------
 -- Records of resource_menu
 -- ----------------------------
 BEGIN;
-INSERT INTO `resource_menu` VALUES (1, 'workbench', '工作台', 'workbench', 1, 'icon-gongzuotai', 0, NULL);
-INSERT INTO `resource_menu` VALUES (2, 'operations', '平台运维', 'operations', 2, 'icon-pingtaiyunwei', 0, NULL);
-INSERT INTO `resource_menu` VALUES (3, 'management', '系统管理', 'management', 3, 'icon-guanlizhongxin', 0, NULL);
-INSERT INTO `resource_menu` VALUES (4, 'spaceOverview', '空间概览', 'spaceOverview', 11, NULL, 1, NULL);
-INSERT INTO `resource_menu` VALUES (5, 'instanceList', '实例列表', 'instanceList', 13, NULL, 1, NULL);
-INSERT INTO `resource_menu` VALUES (6, 'outboundRoute', '对外路由', 'outboundRoute', 14, NULL, 1, NULL);
-INSERT INTO `resource_menu` VALUES (7, 'platformOverview', '资源总览', 'platformOverview', 21, NULL, 2, NULL);
-INSERT INTO `resource_menu` VALUES (8, 'basicResource', '基础资源', 'basicResource', 31, NULL, 3, NULL);
-INSERT INTO `resource_menu` VALUES (10, 'operationAudit', '操作审计', 'operationAudit', 33, NULL, 3, NULL);
-INSERT INTO `resource_menu` VALUES (11, 'userManage', '用户管理', 'userManage', 34, NULL, 3, NULL);
-INSERT INTO `resource_menu` VALUES (12, 'serviceCatalog', '服务目录', 'serviceCatalog', 12, NULL, 1, NULL);
+INSERT INTO `resource_menu` VALUES (1, 'dataOverview', '数据总览', 'dataOverview', 1, 'icon-shujuzonglan', 0, NULL);
+INSERT INTO `resource_menu` VALUES (2, 'middlewareRepository', '中间件市场', 'middlewareRepository', 2, 'icon-cangku', 0, NULL);
+INSERT INTO `resource_menu` VALUES (3, 'serviceList', '服务列表', 'serviceList', 3, 'icon-fuwuliebiao', 0, NULL);
+INSERT INTO `resource_menu` VALUES (4, 'serviceAvailable', '服务暴露', 'serviceAvailable', 4, 'icon-fuwutiaokuan', 0, NULL);
+INSERT INTO `resource_menu` VALUES (5, 'monitorAlarm', '监控告警', 'monitorAlarm', 5, 'icon-gaojingshijian', 0, NULL);
+INSERT INTO `resource_menu` VALUES (6, 'disasterBackup', '容灾备份', 'disasterBackup', 6, 'icon-rongzaibeifen', 0, NULL);
+INSERT INTO `resource_menu` VALUES (7, 'systemManagement', '系统管理', 'systemManagement', 7, 'icon-shezhi01', 0, NULL);
+INSERT INTO `resource_menu` VALUES (8, 'dataMonitor', '数据监控', 'monitorAlarm/dataMonitor', 51, NULL, 5, NULL);
+INSERT INTO `resource_menu` VALUES (9, 'logDetail', '日志详情', 'monitorAlarm/logDetail', 52, NULL, 5, NULL);
+INSERT INTO `resource_menu` VALUES (10, 'alarmCenter', '告警中心', 'monitorAlarm/alarmCenter', 53, NULL, 5, NULL);
+INSERT INTO `resource_menu` VALUES (11, 'disasterCenter', '灾备中心', 'disasterBackup/disasterCenter', 61, NULL, 6, NULL);
+INSERT INTO `resource_menu` VALUES (12, 'dataSecurity', '数据安全', 'disasterBackup/dataSecurity', 62, NULL, 6, NULL);
+INSERT INTO `resource_menu` VALUES (13, 'userManagement', '用户管理', 'systemManagement/userManagement', 71, NULL, 7, NULL);
+INSERT INTO `resource_menu` VALUES (14, 'roleManagement', '角色管理', 'systemManagement/roleManagement', 72, NULL, 7, NULL);
+INSERT INTO `resource_menu` VALUES (15, 'operationAudit', '操作审计', 'systemManagement/operationAudit', 73, NULL, 7, NULL);
+INSERT INTO `resource_menu` VALUES (16, 'resourcePoolManagement', '资源池管理', 'systemManagement/resourcePoolManagement', 74, NULL, 7, NULL);
 COMMIT;
 
 -- ----------------------------
@@ -191,60 +221,28 @@ CREATE TABLE `resource_menu_role` (
   `resource_menu_id` int(11) DEFAULT NULL,
   `available` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='资源菜单角色关联表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='资源菜单角色关联表';
 
 -- ----------------------------
 -- Records of resource_menu_role
 -- ----------------------------
 BEGIN;
-INSERT INTO `resource_menu_role` VALUES (12, 1, 1, 1);
-INSERT INTO `resource_menu_role` VALUES (13, 1, 2, 1);
-INSERT INTO `resource_menu_role` VALUES (14, 1, 3, 1);
-INSERT INTO `resource_menu_role` VALUES (15, 1, 4, 1);
-INSERT INTO `resource_menu_role` VALUES (16, 1, 5, 1);
-INSERT INTO `resource_menu_role` VALUES (17, 1, 6, 1);
-INSERT INTO `resource_menu_role` VALUES (18, 1, 7, 1);
-INSERT INTO `resource_menu_role` VALUES (19, 1, 8, 1);
-INSERT INTO `resource_menu_role` VALUES (20, 1, 9, 1);
-INSERT INTO `resource_menu_role` VALUES (21, 1, 10, 1);
-INSERT INTO `resource_menu_role` VALUES (22, 1, 11, 1);
-INSERT INTO `resource_menu_role` VALUES (23, 2, 1, 1);
-INSERT INTO `resource_menu_role` VALUES (24, 2, 2, 1);
-INSERT INTO `resource_menu_role` VALUES (25, 2, 3, 1);
-INSERT INTO `resource_menu_role` VALUES (26, 2, 4, 1);
-INSERT INTO `resource_menu_role` VALUES (27, 2, 5, 1);
-INSERT INTO `resource_menu_role` VALUES (28, 2, 6, 1);
-INSERT INTO `resource_menu_role` VALUES (29, 2, 7, 1);
-INSERT INTO `resource_menu_role` VALUES (30, 2, 8, 1);
-INSERT INTO `resource_menu_role` VALUES (31, 2, 9, 1);
-INSERT INTO `resource_menu_role` VALUES (32, 2, 10, 1);
-INSERT INTO `resource_menu_role` VALUES (33, 2, 11, 1);
-INSERT INTO `resource_menu_role` VALUES (34, 3, 1, 1);
-INSERT INTO `resource_menu_role` VALUES (35, 3, 2, 1);
-INSERT INTO `resource_menu_role` VALUES (36, 3, 3, 0);
-INSERT INTO `resource_menu_role` VALUES (37, 3, 4, 1);
-INSERT INTO `resource_menu_role` VALUES (38, 3, 5, 1);
-INSERT INTO `resource_menu_role` VALUES (39, 3, 6, 1);
-INSERT INTO `resource_menu_role` VALUES (40, 3, 7, 1);
-INSERT INTO `resource_menu_role` VALUES (41, 3, 8, 0);
-INSERT INTO `resource_menu_role` VALUES (42, 3, 9, 0);
-INSERT INTO `resource_menu_role` VALUES (43, 3, 10, 0);
-INSERT INTO `resource_menu_role` VALUES (44, 3, 11, 0);
-INSERT INTO `resource_menu_role` VALUES (45, 4, 1, 1);
-INSERT INTO `resource_menu_role` VALUES (46, 4, 2, 0);
-INSERT INTO `resource_menu_role` VALUES (47, 4, 3, 0);
-INSERT INTO `resource_menu_role` VALUES (48, 4, 4, 1);
-INSERT INTO `resource_menu_role` VALUES (49, 4, 5, 1);
-INSERT INTO `resource_menu_role` VALUES (50, 4, 6, 1);
-INSERT INTO `resource_menu_role` VALUES (51, 4, 7, 0);
-INSERT INTO `resource_menu_role` VALUES (52, 4, 8, 0);
-INSERT INTO `resource_menu_role` VALUES (53, 4, 9, 0);
-INSERT INTO `resource_menu_role` VALUES (54, 4, 10, 0);
-INSERT INTO `resource_menu_role` VALUES (55, 4, 11, 0);
-INSERT INTO `resource_menu_role` VALUES (56, 1, 12, 1);
-INSERT INTO `resource_menu_role` VALUES (57, 2, 12, 1);
-INSERT INTO `resource_menu_role` VALUES (58, 3, 12, 1);
-INSERT INTO `resource_menu_role` VALUES (59, 4, 12, 1);
+INSERT INTO `resource_menu_role` VALUES (1, 1, 1, 1);
+INSERT INTO `resource_menu_role` VALUES (2, 1, 2, 1);
+INSERT INTO `resource_menu_role` VALUES (3, 1, 3, 1);
+INSERT INTO `resource_menu_role` VALUES (4, 1, 4, 1);
+INSERT INTO `resource_menu_role` VALUES (5, 1, 5, 1);
+INSERT INTO `resource_menu_role` VALUES (6, 1, 6, 1);
+INSERT INTO `resource_menu_role` VALUES (7, 1, 7, 1);
+INSERT INTO `resource_menu_role` VALUES (8, 1, 8, 1);
+INSERT INTO `resource_menu_role` VALUES (9, 1, 9, 1);
+INSERT INTO `resource_menu_role` VALUES (10, 1, 10, 1);
+INSERT INTO `resource_menu_role` VALUES (11, 1, 11, 1);
+INSERT INTO `resource_menu_role` VALUES (12, 1, 12, 1);
+INSERT INTO `resource_menu_role` VALUES (13, 1, 13, 1);
+INSERT INTO `resource_menu_role` VALUES (14, 1, 14, 1);
+INSERT INTO `resource_menu_role` VALUES (15, 1, 15, 1);
+INSERT INTO `resource_menu_role` VALUES (16, 1, 16, 1);
 COMMIT;
 
 -- ----------------------------
@@ -255,18 +253,17 @@ CREATE TABLE `role` (
   `id` int(64) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '名称',
   `description` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',
+  `parent` int(64) DEFAULT NULL COMMENT '父角色id',
   `status` tinyint(1) DEFAULT NULL COMMENT '是否已被删除',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='角色表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='角色表';
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
 BEGIN;
-INSERT INTO `role` VALUES (1, 'admin', '超级管理员', NULL);
-INSERT INTO `role` VALUES (2, '系统管理员', '系统最高权限账户', NULL);
-INSERT INTO `role` VALUES (3, '运维管理员', '负责平台运维', NULL);
-INSERT INTO `role` VALUES (4, '平台管理员', '负责具体中间件业务相关功能版块操作', NULL);
+INSERT INTO `role` VALUES (1, '超级管理员', '拥有所有最高权限', NULL, NULL, NULL);
 COMMIT;
 
 -- ----------------------------
@@ -299,6 +296,8 @@ CREATE TABLE `user` (
   `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '邮箱',
   `phone` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '手机',
   `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `password_time` timestamp NULL DEFAULT NULL COMMENT '密码修改时间',
+  `creator` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建者',
   PRIMARY KEY (`id`,`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户表';
 
@@ -306,20 +305,7 @@ CREATE TABLE `user` (
 -- Records of user
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` VALUES (1, 'admin', 'admin', '5B99164F828AED74140E5FDA077B634C', NULL, NULL, NULL);
+INSERT INTO `user` VALUES (1, 'admin', '超级管理员', '5B99164F828AED74140E5FDA077B634C', NULL, NULL, NULL, NULL, NULL);
 COMMIT;
-
--- ----------------------------
--- Table structure for k8s_default_cluster
--- ----------------------------
-DROP TABLE IF EXISTS `k8s_default_cluster`;
-CREATE TABLE `k8s_default_cluster` (
-  `id` int(2) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `cluster_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '集群id',
-  `url` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '路径',
-  `token` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'service account',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
 
 SET FOREIGN_KEY_CHECKS = 1;
