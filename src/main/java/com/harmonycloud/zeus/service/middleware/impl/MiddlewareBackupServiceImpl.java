ef46d976c4ab4ca53976c8b11ffcee39c7034cac
liyinlong <liyinlong>
2021-10-25 17:45:42 +0800

    fix-修复数据安全已知问题

--- src/main/java/com/harmonycloud/zeus/service/middleware/impl/MiddlewareBackupServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/middleware/impl/MiddlewareBackupServiceImpl.java
@@ -267,21 +267,14 @@ public class MiddlewareBackupServiceImpl implements MiddlewareBackupService {
         middleware.setName(restoreName);
         middleware.setClusterId(clusterId);
         MiddlewareClusterDTO cluster = clusterService.findByIdAndCheckRegistry(middleware.getClusterId());
+        baseOperator.createPreCheck(middleware, cluster);
         //设置中间件恢复信息
         try {
             middleware.setName(restoreName);
             middleware.setAliasName(aliasName);
             middleware.setChartName(type);
-            try {
-                baseOperator.createPreCheck(middleware, cluster);
-            } catch (BusinessException e) {
-                if (e.getCode() == ErrorMessage.EXIST.getCode()) {
-                    middlewareManageTask.asyncCreateBackupRestore(clusterId, namespace, type, middlewareName, backupName, restoreName, this);
-                    return BaseResult.ok();
-                }
-            }
             middlewareService.create(middleware);
-            middlewareManageTask.asyncCreateBackupRestore(clusterId, namespace, type, middlewareName, backupName, restoreName, this);
+            middlewareManageTask.asyncCreateBackupRestore(clusterId, namespace, type, middlewareName, backupName, restoreName,this);
             return BaseResult.ok();
         } catch (Exception e) {
             log.error("备份服务创建失败", e);
