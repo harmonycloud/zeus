649571165f1bf029f49cdb69abcd31194130d885
liyinlong <liyinlong>
2021-11-23 20:26:45 +0800

    feat-middleware_info表新增字段compatible_versions

--- sql/upgrade/v0.2.0/upgrade.sql
+++ sql/upgrade/v0.2.0/upgrade.sql
@@ -64,3 +64,7 @@ CREATE TABLE `ding_robot_info` (
   `creat_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- middleware_info表新增字段compatible_versions
+ALTER TABLE `middleware_info`
+ADD COLUMN compatible_versions varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '升级所需最低版本';
