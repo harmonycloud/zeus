e4d403dbebec78bd98e00494ac7a99e0f78d47f4
xutianhong <736950061@qq.com>
2021-12-06 10:20:07 +0800

    update - sql 更新

--- deploy/docker-compose/conf/mysql/init/middleware.sql
+++ deploy/docker-compose/conf/mysql/init/middleware.sql
@@ -28,7 +28,7 @@ CREATE TABLE `alert_record` (
   `level` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '告警等级',
   `time` timestamp NULL DEFAULT NULL COMMENT '告警时间',
   `type` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '中间件类型',
-  `lay` varchar(16) DEFAULT NULL COMMENT '告警层面',
+  `lay` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '告警层面',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='告警记录表';
 
@@ -43,6 +43,62 @@ CREATE TABLE `alert_rule` (
   `alert` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'prometheusRule内容',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='告警规则表';
+
+-- ----------------------------
+-- Table structure for alert_rule_id
+-- ----------------------------
+DROP TABLE IF EXISTS `alert_rule_id`;
+CREATE TABLE `alert_rule_id` (
+  `alert_id` int NOT NULL AUTO_INCREMENT COMMENT '规则ID',
+  `cluster_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '集群ID',
+  `namespace` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '命名空间',
+  `middleware_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '中间件名称',
+  `alert` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '规则名称',
+  `name` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `silence` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '沉默时间',
+  `symbol` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '符号',
+  `threshold` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '阈值',
+  `time` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '分钟周期',
+  `type` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '中间件类型',
+  `unit` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '单位',
+  `expr` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '执行规则',
+  `description` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '监控项',
+  `annotations` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '备注',
+  `labels` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '标签',
+  `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `alert_time` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '告警时间',
+  `alert_times` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '告警次数',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `status` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '状态',
+  `enable` int DEFAULT NULL COMMENT '是否启用',
+  `content` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '告警内容',
+  `lay` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT 'system 系统告警 service 服务告警',
+  PRIMARY KEY (`alert_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for cluster_components
+-- ----------------------------
+DROP TABLE IF EXISTS `cluster_components`;
+CREATE TABLE `cluster_components` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增Id',
+  `cluster_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '集群Id',
+  `component` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '组件名称',
+  `status` int DEFAULT NULL COMMENT '0-未安装接入 1-已接入 2-安装中 3-运行正常 4-运行异常 5-卸载中',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='集群组件表';
+
+-- ----------------------------
+-- Table structure for cluster_ingress_components
+-- ----------------------------
+DROP TABLE IF EXISTS `cluster_ingress_components`;
+CREATE TABLE `cluster_ingress_components` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'ingress class name',
+  `cluster_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '集群id',
+  `status` int DEFAULT NULL COMMENT '状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='集群ingress组件表';
 
 -- ----------------------------
 -- Table structure for cluster_middleware_info
@@ -106,6 +162,18 @@ CREATE TABLE `custom_config_template` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='自定义配置模板表';
 
 -- ----------------------------
+-- Table structure for ding_robot_info
+-- ----------------------------
+DROP TABLE IF EXISTS `ding_robot_info`;
+CREATE TABLE `ding_robot_info` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `webhook` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `secret_key` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '加签密钥',
+  `creat_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
 -- Table structure for k8s_default_cluster
 -- ----------------------------
 DROP TABLE IF EXISTS `k8s_default_cluster`;
@@ -127,6 +195,36 @@ CREATE TABLE `kube_config` (
   `conf` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'admin.conf',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for mail_info
+-- ----------------------------
+DROP TABLE IF EXISTS `mail_info`;
+CREATE TABLE `mail_info` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `mail_server` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱服务器',
+  `port` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '端口',
+  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '用户',
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '密码',
+  `mail_path` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱地址',
+  `creat_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for mail_to_user
+-- ----------------------------
+DROP TABLE IF EXISTS `mail_to_user`;
+CREATE TABLE `mail_to_user` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `user_id` int DEFAULT NULL COMMENT '用户ID',
+  `username` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '用户',
+  `alias_name` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '账户',
+  `email` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '电话',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Table structure for middleware_info
@@ -185,6 +283,28 @@ CREATE TABLE `operation_audit` (
   KEY `index_url` (`url`(32)) USING BTREE,
   KEY `index_remote_ip` (`remote_ip`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='操作审计';
+
+-- ----------------------------
+-- Table structure for personal_config
+-- ----------------------------
+DROP TABLE IF EXISTS `personal_config`;
+CREATE TABLE `personal_config` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `background_image` mediumblob COMMENT '背景图',
+  `background_image_path` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '背景图地址',
+  `login_logo` mediumblob COMMENT '登录页logo',
+  `login_logo_path` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '登录页logo地址',
+  `home_logo` mediumblob COMMENT '主页logo',
+  `home_logo_path` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '主页logo地址',
+  `platform_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '平台名称',
+  `slogan` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '标语',
+  `copyright_notice` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '版权声明',
+  `title` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '浏览器标题',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  `status` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '是否默认',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Table structure for resource_menu
@@ -322,90 +442,3 @@ INSERT INTO `user` VALUES (1, 'admin', '超级管理员', '6DA05F9A0ED31ABEEFD41
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
-
--- 新增个性化配置表
-DROP TABLE IF EXISTS `personal_config`;
-CREATE TABLE `personal_config` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `background_image` mediumblob COMMENT '背景图',
-  `background_image_path` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '背景图地址',
-  `login_logo` mediumblob COMMENT '登录页logo',
-  `login_logo_path` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '登录页logo地址',
-  `home_logo` mediumblob COMMENT '主页logo',
-  `home_logo_path` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '主页logo地址',
-  `platform_name` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '平台名称',
-  `slogan` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '标语',
-  `copyright_notice` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '版权声明',
-  `title` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '浏览器标题',
-  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` timestamp NULL DEFAULT NULL COMMENT '修改时间',
-  `status` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '是否默认',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- 新增邮箱信息表
-DROP TABLE IF EXISTS `mail_info`;
-CREATE TABLE `mail_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `mail_server` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱服务器',
-  `port` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '端口',
-  `username` varchar(32) COLLATE utf8_bin DEFAULT NULL COMMENT '用户',
-  `password` varchar(32) COLLATE utf8_bin DEFAULT NULL COMMENT '密码',
-  `mail_path` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱地址',
-  `creat_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- 新增被通知人表
-DROP TABLE IF EXISTS `mail_to_user`;
-CREATE TABLE `mail_to_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `user_id` int(11) DEFAULT NULL COMMENT '用户ID',
-  `username` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '用户',
-  `alias_name` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '账户',
-  `email` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱',
-  `phone` varchar(32) COLLATE utf8_bin DEFAULT NULL COMMENT '电话',
-  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- 新增钉钉机器人表
-DROP TABLE IF EXISTS `ding_robot_info`;
-CREATE TABLE `ding_robot_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
-  `webhook` varchar(128) COLLATE utf8_bin NOT NULL,
-  `secret_key` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '加签密钥',
-  `enable_ding` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '是否启用该钉钉机器人 1 启用 0 否',
-  `creat_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- 新增告警规则表
-DROP TABLE IF EXISTS `alert_rule_id`;
-CREATE TABLE `alert_rule_id` (
-  `alert_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '规则ID',
-  `cluster_id` varchar(32) COLLATE utf8_bin DEFAULT NULL COMMENT '集群ID',
-  `namespace` varchar(32) COLLATE utf8_bin DEFAULT NULL COMMENT '命名空间',
-  `middleware_name` varchar(32) COLLATE utf8_bin DEFAULT NULL COMMENT '中间件名称',
-  `alert` text COLLATE utf8_bin COMMENT '规则名称',
-  `name` varchar(16) COLLATE utf8_bin DEFAULT NULL,
-  `silence` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '沉默时间',
-  `symbol` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '符号',
-  `threshold` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '阈值',
-  `time` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '分钟周期',
-  `type` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '中间件类型',
-  `unit` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '单位',
-  `expr` varchar(256) COLLATE utf8_bin DEFAULT NULL COMMENT '执行规则',
-  `description` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '监控项',
-  `annotations` text COLLATE utf8_bin COMMENT '备注',
-  `labels` text COLLATE utf8_bin COMMENT '标签',
-  `id` varchar(32) COLLATE utf8_bin DEFAULT NULL,
-  `alert_time` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '告警时间',
-  `alert_times` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '告警次数',
-  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `status` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '状态',
-  `enable` int(11) DEFAULT NULL COMMENT '是否启用',
-  `content` varchar(32) COLLATE utf8_bin DEFAULT NULL COMMENT '告警内容',
-  `lay` varchar(12) COLLATE utf8_bin DEFAULT NULL COMMENT 'system 系统告警 service 服务告警',
-  PRIMARY KEY (`alert_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
