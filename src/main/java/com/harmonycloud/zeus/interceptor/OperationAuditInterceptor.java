eb9214162d6f375792ed7e5da959707a50619310
liyinlong <liyinlong>
2022-01-06 17:44:06 +0800

    fix-查询备份列表错误

--- src/main/java/com/harmonycloud/zeus/interceptor/OperationAuditInterceptor.java
+++ src/main/java/com/harmonycloud/zeus/interceptor/OperationAuditInterceptor.java
@@ -212,14 +212,9 @@ public class OperationAuditInterceptor {
      * @param operationAudit
      */
     public void setRealIp(HttpServletRequest request, BeanOperationAudit operationAudit) {
-        String remoteRealIp = request.getHeader("Remote-Real-IP");
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()){
-            String name = headerNames.nextElement();
-            log.info(name + "----->" + request.getHeader(name));
-        }
-        log.info("remoteRealIp:{}", remoteRealIp);
-        log.info("remoteHost:{}", request.getRemoteHost());
+        String remoteRealIp = request.getHeader("remote-real-ip");
+        log.info("remoterealip:{}", remoteRealIp);
+        log.info("remotehost:{}", request.getRemoteHost());
         if (remoteRealIp != null && !StringUtils.isBlank(remoteRealIp)) {
             operationAudit.setRemoteIp(remoteRealIp);
         } else {
