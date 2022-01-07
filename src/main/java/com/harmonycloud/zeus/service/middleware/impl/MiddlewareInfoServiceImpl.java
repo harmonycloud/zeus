0b8518e5150937d3bdb023928eb8cd26e008d0bc
xutianhong <736950061@qq.com>
2022-01-07 15:42:17 +0800

    fix - 中间件版本排序优化

--- src/main/java/com/harmonycloud/zeus/service/middleware/impl/MiddlewareInfoServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/middleware/impl/MiddlewareInfoServiceImpl.java
@@ -72,7 +72,7 @@ public class MiddlewareInfoServiceImpl implements MiddlewareInfoService {
                 list.stream().collect(Collectors.groupingBy(BeanMiddlewareInfo::getChartName));
             List<BeanMiddlewareInfo> mwInfoList = new ArrayList<>();
             for (String key : map.keySet()) {
-                map.get(key).sort(Comparator.comparing(BeanMiddlewareInfo::getChartVersion).reversed());
+                compareChartVersion(map.get(key));
                 mwInfoList.add(map.get(key).get(0));
             }
             return mwInfoList;
@@ -122,7 +122,7 @@ public class MiddlewareInfoServiceImpl implements MiddlewareInfoService {
                         clusterMwInfo -> clusterMwInfo.getChartName().equals(mwInfoMap.get(key).get(0).getChartName()))) {
                     //获取最新版本的中间件写入集群中间件关联关系
                     List<BeanMiddlewareInfo> list = mwInfoMap.get(key);
-                    list.sort(Comparator.comparing(BeanMiddlewareInfo::getChartVersion).reversed());
+                    compareChartVersion(list);
                     BeanClusterMiddlewareInfo clusterMwInfo = new BeanClusterMiddlewareInfo();
                     clusterMwInfo.setClusterId(clusterId);
                     clusterMwInfo.setChartName(list.get(0).getChartName());
@@ -416,7 +416,38 @@ public class MiddlewareInfoServiceImpl implements MiddlewareInfoService {
         QueryWrapper<BeanMiddlewareInfo> wrapper = new QueryWrapper<BeanMiddlewareInfo>().eq("chart_name", type);
         List<BeanMiddlewareInfo> mwInfoList = middlewareInfoMapper.selectList(wrapper);
         // 根据版本倒序排列
-        mwInfoList.sort(Comparator.comparing(BeanMiddlewareInfo::getChartVersion).reversed());
+        compareChartVersion(mwInfoList);
         return mwInfoList;
+    }
+
+    public void compareChartVersion(List<BeanMiddlewareInfo> mwInfoList){
+        mwInfoList.sort((o1, o2) -> {
+            //根据 "-" 进行分隔
+            String[] tempO1 = o1.getChartVersion().split("-");
+            String[] tempO2 = o2.getChartVersion().split("-");
+            //根据 "." 进行分隔
+            String[] numO1 = tempO1[0].split("\\.");
+            String[] numO2 = tempO2[0].split("\\.");
+            // 先判断"-"前 如：2.3.4 和 1.5.6
+            int size = Math.min(numO1.length, numO2.length);
+            for (int i = 0; i < size; ++i){
+                int intO1 = Integer.parseInt(numO1[i]);
+                int intO2 = Integer.parseInt(numO2[i]);
+                if (intO1 == intO2){
+                    continue;
+                }
+                return Integer.compare(intO2, intO1);
+            }
+            // 判断3.0和3.0.1
+            if (numO1.length != numO2.length){
+                return Integer.compare(numO2.length, numO1.length);
+            }
+            // 判断1.5.6和1.5.6-1
+            if (tempO1.length != tempO2.length){
+                return Integer.compare(tempO2.length, tempO1.length);
+            }
+            // 判断1.5.6-2和1.5.6-3
+            return tempO2[1].compareTo(tempO1[1]);
+        });
     }
 }
