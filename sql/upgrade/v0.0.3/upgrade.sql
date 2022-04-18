66473f5aab21e96922bec420551f8896c249ec86
xutianhong <736950061@qq.com>
2022-04-18 17:36:47 +0800

    feat - delete upgrade sql

--- sql/upgrade/v0.0.3/upgrade.sql
+++
@@ -1,9 +0,0 @@
--- 2021.9.24 xutianhong
--- 添加记录admin.conf
-DROP TABLE IF EXISTS `kube_config`;
-CREATE TABLE `kube_config` (
-  `id` int(32) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `cluster_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '集群id',
-  `conf` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'admin.conf',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- sql/upgrade/v0.3.0/upgrade.sql
+++
@@ -1,4 +0,0 @@
--- 2022.2.23 liyinlong
--- 请求ip字段长度改为128
-ALTER TABLE `operation_audit`
-    MODIFY COLUMN `remote_ip` char(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '请求ip' AFTER `response`;
