cdc62aee505f9aec20d3afc34d333d9d8601dec6
xutianhong <736950061@qq.com>
2022-05-24 09:43:03 +0800

    update - sql upgrade

--- deploy/docker-compose/conf/mysql/init/middleware.sql
+++ deploy/docker-compose/conf/mysql/init/middleware.sql
@@ -991,6 +991,7 @@ create table `active_area` (
     `cluster_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL comment '集群id',
     `area_name`  varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL comment '可用区名称',
     `alias_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL comment '可用区中文别名',
+    `init` tinyint(1) DEFAULT NULL COMMENT '是否初始化',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin comment '可用区'
 
