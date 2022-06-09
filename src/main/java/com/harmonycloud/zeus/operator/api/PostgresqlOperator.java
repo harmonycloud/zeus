246ba9157966e6563076396a63ed7e30980ed1ab
xutianhong <736950061@qq.com>
2022-06-09 11:02:47 +0800

    feat - pg固化

---
+++ src/main/java/com/harmonycloud/zeus/operator/api/PostgresqlOperator.java
@@ -0,0 +1,8 @@
+package com.harmonycloud.zeus.operator.api;
+
+/**
+ * @author xutianhong
+ * @Date 2022/6/7 2:55 下午
+ */
+public interface PostgresqlOperator {
+}
---
+++ src/main/java/com/harmonycloud/zeus/operator/impl/PostgresqlOperatorImpl.java
@@ -0,0 +1,86 @@
+package com.harmonycloud.zeus.operator.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.harmonycloud.caas.common.model.middleware.Middleware;
+import com.harmonycloud.caas.common.model.middleware.MiddlewareClusterDTO;
+import com.harmonycloud.caas.common.model.middleware.MiddlewareQuota;
+import com.harmonycloud.tool.encrypt.PasswordUtils;
+import com.harmonycloud.zeus.annotation.Operator;
+import com.harmonycloud.zeus.operator.api.PostgresqlOperator;
+import com.harmonycloud.zeus.operator.miiddleware.AbstractPostgresqlOperator;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import static com.harmonycloud.caas.common.constants.NameConstant.RESOURCES;
+
+/**
+ * @author xutianhong
+ * @Date 2022/6/7 3:19 下午
+ */
+@Slf4j
+@Operator(paramTypes4One = Middleware.class)
+public class PostgresqlOperatorImpl extends AbstractPostgresqlOperator implements PostgresqlOperator {
+
+    @Override
+    public void replaceValues(Middleware middleware, MiddlewareClusterDTO cluster, JSONObject values) {
+        // 替换通用values
+        replaceCommonValues(middleware, cluster, values);
+        MiddlewareQuota quota = middleware.getQuota().get(middleware.getType());
+        replaceCommonResources(quota, values.getJSONObject(RESOURCES));
+        replaceCommonStorages(quota, values);
+
+        // 替换pgSQL专用
+        // 替换实例数
+        values.put("instances", quota.getNum());
+        // 替换密码
+        if (StringUtils.isBlank(middleware.getPassword())) {
+            middleware.setPassword(PasswordUtils.generateCommonPassword(10));
+        }
+        JSONObject userPasswords = new JSONObject();
+        userPasswords.put("postgres", middleware.getPassword());
+        values.put("userPasswords", userPasswords);
+
+    }
+
+    @Override
+    public Middleware convertByHelmChart(Middleware middleware, MiddlewareClusterDTO cluster) {
+        JSONObject values = helmChartService.getInstalledValues(middleware, cluster);
+        convertCommonByHelmChart(middleware, values);
+        convertStoragesByHelmChart(middleware, middleware.getType(), values);
+        convertRegistry(middleware, cluster);
+
+        return middleware;
+    }
+
+    @Override
+    public void update(Middleware middleware, MiddlewareClusterDTO cluster) {
+        StringBuilder sb = new StringBuilder();
+
+        // 实例扩容
+        if (middleware.getQuota() != null && middleware.getQuota().get(middleware.getType()) != null) {
+            MiddlewareQuota quota = middleware.getQuota().get(middleware.getType());
+            // 设置limit的resources
+            setLimitResources(quota);
+            if (StringUtils.isNotBlank(quota.getCpu())) {
+                sb.append("resources.requests.cpu=").append(quota.getCpu()).append(",resources.limits.cpu=")
+                        .append(quota.getLimitCpu()).append(",");
+            }
+            if (StringUtils.isNotBlank(quota.getMemory())) {
+                sb.append("resources.requests.memory=").append(quota.getMemory()).append(",resources.limits.memory=")
+                        .append(quota.getLimitMemory()).append(",");
+            }
+        }
+        updateCommonValues(sb, middleware);
+        // 没有修改，直接返回
+        if (sb.length() == 0) {
+            return;
+        }
+        // 去掉末尾的逗号
+        sb.deleteCharAt(sb.length() - 1);
+        // 更新helm
+        helmChartService.upgrade(middleware, sb.toString(), cluster);
+    }
+
+
+
+}
