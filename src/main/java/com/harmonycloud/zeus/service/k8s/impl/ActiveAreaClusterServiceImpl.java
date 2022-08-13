a10cd1fd7d98770951129682c57797c122459c6f
xutianhong <736950061@qq.com>
2022-08-13 15:23:58 +0800

    feat - 双活集群内容开发

---
+++ src/main/java/com/harmonycloud/zeus/service/k8s/impl/ActiveAreaClusterServiceImpl.java
@@ -0,0 +1,42 @@
+package com.harmonycloud.zeus.service.k8s.impl;
+
+import com.harmonycloud.caas.common.model.ActiveAreaClusterDto;
+import com.harmonycloud.zeus.service.k8s.ActiveAreaClusterService;
+import com.harmonycloud.zeus.service.k8s.ClusterService;
+import com.harmonycloud.zeus.service.k8s.MiddlewareClusterService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author xutianhong
+ * @Date 2022/8/13 1:44 下午
+ */
+@Service
+@Slf4j
+public class ActiveAreaClusterServiceImpl implements ActiveAreaClusterService {
+
+    @Autowired
+    private ClusterService clusterService;
+
+    @Override
+    public List<ActiveAreaClusterDto> list() {
+        return clusterService.listClusters().stream().map(cluster -> {
+            ActiveAreaClusterDto activeAreaClusterDto = new ActiveAreaClusterDto()
+                    .setClusterId(cluster.getId())
+                    .setClusterAliasName(cluster.getNickname())
+                    .setActiveActive(cluster.getActiveActive())
+                    .setActiveAreaNum(3)
+                    .setStatusCode(1);
+            return activeAreaClusterDto;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public void update(String clusterId, Boolean active) {
+
+    }
+}
