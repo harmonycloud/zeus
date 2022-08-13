9a93b117010800556ea7cc6ae53d17aa910d3196
xutianhong <736950061@qq.com>
2022-08-13 13:11:01 +0800

    feat - sql更新

--- deploy/docker-compose/conf/mysql/init/middleware.sql
+++ deploy/docker-compose/conf/mysql/init/middleware.sql
@@ -1176,6 +1176,15 @@ LOCK TABLES `system_config` WRITE;
 /*!40000 ALTER TABLE `system_config` ENABLE KEYS */;
 UNLOCK TABLES;
 
+DROP TABLE IF EXISTS `active_area`;
+create table `active_area` (
+    `id` int NOT NULL auto_increment comment '自增id',
+    `cluster_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL comment '集群id',
+    `alias_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL comment '可用区中文别名',
+    `init` tinyint(1) DEFAULT NULL COMMENT '是否初始化',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin comment '可用区'
+
 --
 -- Table structure for table `user`
 --
