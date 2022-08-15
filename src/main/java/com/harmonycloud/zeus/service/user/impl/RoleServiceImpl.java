84aedc61491eb7db3200dab0a93094fe845e2fed
liyinlong <liyinlong>
2022-08-15 15:36:52 +0800

    fix-加入同城双活菜单

--- src/main/java/com/harmonycloud/zeus/service/user/impl/RoleServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/user/impl/RoleServiceImpl.java
@@ -154,7 +154,7 @@ public class RoleServiceImpl implements RoleService {
                 }
             }
         } else {
-            ids = Arrays.asList(1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21);
+            ids = Arrays.asList(1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22);
         }
         // 获取菜单信息
         List<ResourceMenuDto> resourceMenuDtoList = resourceMenuService.list(ids);
