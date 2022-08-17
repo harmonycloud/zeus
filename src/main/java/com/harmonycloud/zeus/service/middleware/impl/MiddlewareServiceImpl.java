3072d0784d158e9b6b91294050bbf4f8fdf5efe1
liyinlong <liyinlong>
2022-08-17 16:00:15 +0800

    fix-修复服务控制台点击报错

--- src/main/java/com/harmonycloud/zeus/service/middleware/impl/MiddlewareServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/middleware/impl/MiddlewareServiceImpl.java
@@ -844,18 +844,39 @@ public class MiddlewareServiceImpl extends AbstractBaseService implements Middle
     @Override
     public String platform(String clusterId, String namespace, String name, String type) {
         if (!type.equals(MiddlewareTypeEnum.ELASTIC_SEARCH.getType())
-            && !type.equals(MiddlewareTypeEnum.KAFKA.getType())
-            && !type.equals(MiddlewareTypeEnum.ROCKET_MQ.getType())) {
+                && !type.equals(MiddlewareTypeEnum.KAFKA.getType())
+                && !type.equals(MiddlewareTypeEnum.ROCKET_MQ.getType())) {
             throw new BusinessException(ErrorMessage.MIDDLEWARE_MANAGER_PLATFORM_NOT_SUPPORT);
         }
         List<IngressDTO> ingressDTOS = ingressService.get(clusterId, namespace, type, name);
         String servicePort = ServiceNameConvertUtil.getManagePlatformServicePort(type);
         for (IngressDTO ingressDTO : ingressDTOS) {
+            if (!CollectionUtils.isEmpty(ingressDTO.getRules())) {
+                List<IngressRuleDTO> rules = ingressDTO.getRules();
+                for (IngressRuleDTO rule : rules) {
+                    String domain = rule.getDomain();
+                    List<IngressHttpPath> ingressHttpPaths = rule.getIngressHttpPaths();
+                    if (!CollectionUtils.isEmpty(ingressHttpPaths)) {
+                        for (IngressHttpPath ingressHttpPath : ingressHttpPaths) {
+                            assert servicePort != null;
+                            if (servicePort.equals(ingressHttpPath.getServicePort())) {
+                                return domain + ingressHttpPath.getPath();
+                            }
+                        }
+                    }
+                }
+            }
             List<ServiceDTO> serviceList = ingressDTO.getServiceList();
+            Set<String> ipSet = ingressService.listIngressIp(clusterId, ingressDTO.getIngressClassName());
+            String exposeIp = "";
+            for (String ip : ipSet) {
+                exposeIp = ip;
+                break;
+            }
             if (!CollectionUtils.isEmpty(serviceList)) {
                 for (ServiceDTO serviceDTO : serviceList) {
                     if (serviceDTO.getServicePort().equals(servicePort)) {
-                        return ingressDTO.getExposeIP() + ":" + serviceDTO.getExposePort();
+                        return (StringUtils.isBlank(ingressDTO.getExposeIP()) ? exposeIp : ingressDTO.getExposeIP()) + ":" + serviceDTO.getExposePort();
                     }
                 }
             }
