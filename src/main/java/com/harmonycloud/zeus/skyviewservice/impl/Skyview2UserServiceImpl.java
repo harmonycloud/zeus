57618fb9a359e83b23fe51c32db1b501741d4d92
liyinlong <liyinlong>
2022-09-13 14:52:04 +0800

    fix-修复bean冲突

--- src/main/java/com/harmonycloud/zeus/skyviewservice/impl/Skyview2UserServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/skyviewservice/impl/Skyview2UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.harmonycloud.zeus.skyviewservice.Skyview2UserService;
 import com.harmonycloud.zeus.skyviewservice.client.Skyview2UserServiceClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,7 @@ import org.springframework.stereotype.Service;
  * @author xutianhong
  * @Date 2022/9/13 10:14 上午
  */
-@Service
+@Service("skyview2UserService")
 @Slf4j
 @ConditionalOnProperty(value="system.usercenter",havingValue = "skyview2")
 public class Skyview2UserServiceImpl implements Skyview2UserService {
