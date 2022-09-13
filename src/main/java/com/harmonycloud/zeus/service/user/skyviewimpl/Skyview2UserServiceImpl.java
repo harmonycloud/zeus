04abe18fa7e148a92dcee0a18bb163137ad31e4c
liyinlong <liyinlong>
2022-09-13 18:49:06 +0800

    fix-用户没有租户时登录后报错

--- src/main/java/com/harmonycloud/zeus/service/user/skyviewimpl/Skyview2UserServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/user/skyviewimpl/Skyview2UserServiceImpl.java
@@ -185,7 +185,9 @@ public class Skyview2UserServiceImpl extends UserServiceImpl {
         String username = ZeusCurrentUser.getUserName();
         String caasToken = ZeusCurrentUser.getCaasToken();
         List<ProjectDTO>  projects =  skyview2ProjectService.listAllTenantProject(caasToken);
-
+        if (CollectionUtils.isEmpty(projects)) {
+            return;
+        }
         ExecutorService executorService = Executors.newFixedThreadPool(projects.size());
         CountDownLatch latch = new CountDownLatch(projects.size());
         // 2、获取用户在每个项目下的角色
