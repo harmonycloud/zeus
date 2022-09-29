7733b9cc52ab69a2bd51377adec08b3f33146978
xutianhong <736950061@qq.com>
2022-09-29 13:49:54 +0800

    feat - tcp端口范围获取修改为后端获取

---
+++ src/main/java/com/harmonycloud/zeus/controller/system/TcpPortController.java
@@ -0,0 +1,46 @@
+package com.harmonycloud.zeus.controller.system;
+
+import com.harmonycloud.caas.common.base.BaseResult;
+import com.harmonycloud.caas.common.model.LdapConfigDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xutianhong
+ * @Date 2022/9/26 11:34 上午
+ */
+@Api(tags = {"系统管理", "端口范围"}, value = "端口范围")
+@RestController
+@RequestMapping("/port")
+public class TcpPortController {
+
+    @Value("${server.ingressTcp.start: 30000}")
+    private Integer ingressTcpStart;
+    @Value("${server.ingressTcp.end: 65535}")
+    private Integer ingressTcpEnd;
+    @Value("${server.nodePort.start: 30000}")
+    private Integer nodePortStart;
+    @Value("${server.nodePort.end: 32767}")
+    private Integer nodePortEnd;
+
+
+    @ApiOperation(value = "启用ldap", notes = "启用ldap")
+    @PostMapping("/ingressTcp")
+    public BaseResult<String> ingressTcp() {
+        String port = String.format("%s-%s", ingressTcpStart, ingressTcpEnd);
+        return BaseResult.ok(port);
+    }
+
+    @ApiOperation(value = "启用ldap", notes = "启用ldap")
+    @PostMapping("/nodePort")
+    public BaseResult<String> nodePort() {
+        String port = String.format("%s-%s", nodePortStart, nodePortEnd);
+        return BaseResult.ok(port);
+    }
+
+}
