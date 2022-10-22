1480ecb81c8b596d1c8ddcb5e96f8ab74dff8202
xutianhong <736950061@qq.com>
2022-10-22 14:20:27 +0800

    feat - 修改postgresql路径拼接

--- src/main/java/com/harmonycloud/zeus/service/dashboard/impl/PostgresqlDashboardServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/dashboard/impl/PostgresqlDashboardServiceImpl.java
@@ -970,8 +970,8 @@ public class PostgresqlDashboardServiceImpl implements PostgresqlDashboardServic
     }
 
     public String getPath(String middlewareName, String namespace) {
-        // return middlewareName + "." + namespace;
-        return middlewareName;
+         return middlewareName + "." + namespace;
+        //return middlewareName;
     }
 
 }
