1c286d8e524be950fefd237d01cf75a1bce12ddd
liyinlong <liyinlong>
2022-10-24 10:03:53 +0800

    feat-mq服务用途修改

--- src/main/java/com/harmonycloud/zeus/util/MiddlewareServicePurposeUtil.java
+++ src/main/java/com/harmonycloud/zeus/util/MiddlewareServicePurposeUtil.java
@@ -122,7 +122,7 @@ public class MiddlewareServicePurposeUtil {
         if (serviceName.contains("console-svc")) {
             return "管理页面";
         } else if (serviceName.contains("proxy")) {
-            return "服务代理";
+            return "服务代理proxy-" + serviceName.substring(serviceName.length()-1);
         } else if (serviceName.contains("namesrv")) {
             return "服务连接";
         } else if (serviceName.contains("master") || serviceName.contains("slave")) {
