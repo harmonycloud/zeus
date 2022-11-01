9e4436dae7c39202f1b7b5d4ede4c4a06ece23d2
liyinlong <liyinlong>
2022-11-01 11:25:42 +0800

    fix-服务暴露列表报错

--- src/main/java/com/harmonycloud/zeus/service/k8s/impl/IngressServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/k8s/impl/IngressServiceImpl.java
@@ -1484,8 +1484,13 @@ public class IngressServiceImpl implements IngressService {
         } else {
             address = null;
         }
+        // 过滤掉不属于中间件的ingressRouteTCP CR
+        List<IngressRouteTCPCR> items = ingressRouteTCPList.getItems().stream().filter(ingressRouteTCPCR
+                -> ingressRouteTCPCR.getMetadata().getLabels() != null
+                && ingressRouteTCPCR.getMetadata().getLabels().containsKey("middlewareType")).collect(Collectors.toList());
+
         String finalAddress = address;
-        ingressRouteTCPList.getItems().forEach(ingressRouteTCPCR -> {
+        items.forEach(ingressRouteTCPCR -> {
             List<String> entryPoints = ingressRouteTCPCR.getSpec().getEntryPoints();
             if (!CollectionUtils.isEmpty(entryPoints) && !CollectionUtils.isEmpty(ingressRouteTCPCR.getSpec().getRoutes()) && !CollectionUtils.isEmpty(ingressRouteTCPCR.getSpec().getRoutes().get(0).getServices())) {
                 String entryPoint = entryPoints.get(0);
