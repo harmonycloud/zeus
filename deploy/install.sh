41ab8ea155de8bb6e6da217aaa3dd6ac3c697e90
xutianhong <736950061@qq.com>
2022-11-11 10:56:49 +0800

    update - 部署脚本回滚

--- deploy/install.sh
+++ deploy/install.sh
@@ -16,7 +16,7 @@ fi
 
 function deploy_docker() {
   cd deploy/docker-compose/
-  zeus_repository=$IMAGE_REPO"/" docker-compose -f zeus.yaml up -d
+  zeus_repository=$IMAGE_REPO"/middleware/" docker-compose -f zeus.yaml up -d
 }
 
 function deploy_kubernetes() {
@@ -27,32 +27,20 @@ function deploy_kubernetes() {
 function deploy_helm() {
 
   kubectl apply -f deploy/namespaces.yaml
-  MYSQL_REPLICATE="replicaCount=1"
-  OPERATOR_HELM_SET=""
-  MYSQL_HELM_SET=""
-  if [ $HA == "ha" ]; then
-    MYSQL_REPLICATE="replicaCount=2"
-    OPERATOR_HELM_SET=" --set replicaCount=3"
-  fi
-
-  if [ $HA == "active-active" ]; then
-    MYSQL_REPLICATE="replicaCount=2"
-    OPERATOR_HELM_SET=" -f deploy/mysql-operator/charts/mysql-operator/values-active-active.yaml --set replicaCount=3"
-    MYSQL_HELM_SET=" -f deploy/mysql-operator/values-active-active.yaml "
-  fi
   # install mysql-operator
-  helm install -n middleware-operator mysql-operator  deploy/mysql-operator/charts/mysql-operator --set image.repository=$IMAGE_REPO $OPERATOR_HELM_SET
+  helm install -n middleware-operator mysql-operator deploy/mysql-operator/charts/mysql-operator --set image.repository=$IMAGE_REPO"/middleware"
   # install mysql instance
-  helm install -n zeus zeus-mysql deploy/mysql-operator --set mysql-operator.enabled=false,image.repository=$IMAGE_REPO,args.root_password="ZeuS@Middleware01",storageClassName=$STORAGE_CLASS,$MYSQL_REPLICATE $MYSQL_HELM_SET
+  MYSQL_REPLICATE="replicaCount=1"
+  if [ $HA == "true" ]; then
+    MYSQL_REPLICATE="replicaCount=2"
+  fi
+  helm install -n zeus zeus-mysql deploy/mysql-operator --set mysql-operator.enabled=false,image.repository=$IMAGE_REPO"/middleware",args.root_password="ZeuS@Middleware01",storageClassName=$STORAGE_CLASS,$MYSQL_REPLICATE
   # install zeus platform
   HELM_ARGS="global.replicaCount=1"
-  if [ $HA == "ha" ]; then
+  if [ $HA == "true" ]; then
     HELM_ARGS="global.replicaCount=3"
   fi
-  if [ $HA == "active-active" ]; then
-    HELM_ARGS="global.replicaCount=3 -f deploy/helm/values-active-active.yaml"
-  fi
-  helm install -n zeus zeus deploy/helm --set global.repository=$IMAGE_REPO,global.storageClass=$STORAGE_CLASS,$HELM_ARGS
+  helm install -n zeus zeus deploy/helm --set global.repository=$IMAGE_REPO"/middleware",global.storageClass=$STORAGE_CLASS,$HELM_ARGS
 }
 
 if [ $DEPLOY_TYPE == "docker-compose" ]; then
