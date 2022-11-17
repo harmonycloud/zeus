310102106aaaf6192940c33b4618dadb0fc170c7
xutianhong <736950061@qq.com>
2022-11-17 11:12:09 +0800

    feat - postgresql 修改getPath

--- src/main/java/com/harmonycloud/zeus/service/dashboard/impl/PostgresqlDashboardServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/dashboard/impl/PostgresqlDashboardServiceImpl.java
@@ -1351,8 +1351,8 @@ public class PostgresqlDashboardServiceImpl implements PostgresqlDashboardServic
     }
 
     public String getPath(String middlewareName, String namespace) {
-        //return middlewareName + "." + namespace;
-        return middlewareName;
+        return middlewareName + "." + namespace;
+        //return middlewareName;
     }
 
 }
