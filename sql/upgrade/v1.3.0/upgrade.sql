64e4814729ade1c6e84a4647bb59cef808e48932
liyinlong <liyinlong>
2022-11-17 17:49:46 +0800

    feat-添加SQL执行记录表sql脚本

--- sql/upgrade/v1.3.0/upgrade.sql
+++ sql/upgrade/v1.3.0/upgrade.sql
@@ -2,16 +2,18 @@
 -- 2022.10.21 添加sql执行记录表
 DROP TABLE IF EXISTS `sql_execute_record`;
 CREATE TABLE `sql_execute_record` (
-    `id` int NOT NULL AUTO_INCREMENT COMMENT '自增Id',
-    `database` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '目标database',
-    `sql` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '执行sql',
-    `line` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '行数',
-    `time` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '耗时',
-    `message` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '信息',
-    `status` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '状态',
-    `date` timestamp NULL DEFAULT NULL COMMENT '执行时间',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='sql执行表';
+  `id` int(11) NOT NULL,
+  `cluster_id` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+  `namespace` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+  `middleware_name` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+  `target_database` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+  `sqlstr` varchar(1024) COLLATE utf8_bin DEFAULT NULL COMMENT '执行sql',
+  `status` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '执行状态',
+  `exec_date` timestamp NULL DEFAULT NULL COMMENT '执行时间',
+  `exec_time` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '耗时',
+  `message` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '信息',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT 'sql执行记录表';
 
 -- wangpenglei
 -- 2022.10.27  修改任务名称长度
