d673a359929c39a5ce0d84bb29c5545f20683451
liyinlong <liyinlong>
2022-11-24 19:00:27 +0800

    fix-修改sql脚本

--- sql/upgrade/v1.3.0/upgrade.sql
+++ sql/upgrade/v1.3.0/upgrade.sql
@@ -2,18 +2,18 @@
 -- 2022.10.21 添加sql执行记录表
 DROP TABLE IF EXISTS `sql_execute_record`;
 CREATE TABLE `sql_execute_record` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `cluster_id` varchar(256) COLLATE utf8_bin DEFAULT NULL,
   `namespace` varchar(256) COLLATE utf8_bin DEFAULT NULL,
   `middleware_name` varchar(256) COLLATE utf8_bin DEFAULT NULL,
   `target_database` varchar(256) COLLATE utf8_bin DEFAULT NULL,
   `sqlstr` varchar(1024) COLLATE utf8_bin DEFAULT NULL COMMENT '执行sql',
-  `status` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '执行状态',
+  `exec_status` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '执行状态',
   `exec_date` timestamp NULL DEFAULT NULL COMMENT '执行时间',
   `exec_time` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '耗时',
   `message` varchar(45) COLLATE utf8_bin DEFAULT NULL COMMENT '信息',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT 'sql执行记录表';
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='sql执行记录表';
 
 -- wangpenglei
 -- 2022.10.27  修改任务名称长度
