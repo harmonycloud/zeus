513e44cf21f6e72976a214efd09e5e96759716ac
wangpenglei <wangpenglei2001@163.com>
2023-01-09 14:09:48 +0800

    feat - redis查询分片信息

--- src/main/java/com/harmonycloud/zeus/service/middleware/impl/RedisServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/middleware/impl/RedisServiceImpl.java
@@ -7,8 +7,14 @@ import static com.harmonycloud.zeus.util.RedisUtil.getRedisSentinelIsOk;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import cn.hutool.core.collection.CollectionUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.harmonycloud.caas.common.enums.DictEnum;
 import com.harmonycloud.caas.common.model.Node;
 import com.harmonycloud.caas.common.model.middleware.*;
+import com.harmonycloud.zeus.integration.cluster.bean.MiddlewareCR;
+import com.harmonycloud.zeus.service.k8s.MiddlewareCRService;
 import com.harmonycloud.zeus.service.k8s.NodeService;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -52,6 +58,9 @@ public class RedisServiceImpl extends AbstractMiddlewareService implements Redis
     private RedisOperatorImpl redisOperator;
     @Autowired
     private NodeService nodeService;
+    @Autowired
+    private MiddlewareCRService middlewareCRService;
+
 
     @Override
     public List<RedisDbDTO> listRedisDb(String clusterId, String namespace, String middlewareName, String db,
@@ -415,4 +424,97 @@ public class RedisServiceImpl extends AbstractMiddlewareService implements Redis
         }
     }
 
+
+    @Override
+    public String getBurstMaster(String clusterId, String namespace, String middlewareName, String slaveName, String mode) {
+        if (StringUtils.isBlank(slaveName)) {
+            throw new BusinessException(ErrorMessage.REDIS_INCOMPLETE_PARAMETERS);
+        }
+        MiddlewareCR cr = middlewareCRService.getCR(clusterId, namespace, MiddlewareTypeEnum.REDIS.getType(), middlewareName);
+        JSONObject status = JSONObject.parseObject(cr.getMetadata().getAnnotations().get("status"));
+        JSONArray conditions = status.getJSONArray("conditions");
+        if (CollectionUtil.isEmpty(conditions)){
+            throw new BusinessException(DictEnum.POD,ErrorMessage.NOT_FOUND);
+        }
+
+        // 哨兵模式
+        if ("sentinel".equals(mode)) {
+            // 分片名字
+            String burstName = slaveName.substring(0, slaveName.lastIndexOf("-"));
+            for (Object condition : conditions) {
+                JSONObject con = (JSONObject) condition;
+                String conName = con.getString("name");
+                if (conName.contains(burstName) && !slaveName.equals(conName)) {
+                    return conName;
+                }
+            }
+        } else {
+            String masterNodeId = null;
+            for (Object condition : conditions) {
+                JSONObject con = (JSONObject) condition;
+                if (slaveName.equals(con.getString("name"))) {
+                    masterNodeId = con.getString("masterNodeId");
+                    break;
+                }
+            }
+            if (masterNodeId == null) throw new BusinessException(ErrorMessage.NODE_NOT_FOUND);
+            for (Object condition : conditions) {
+                JSONObject con = (JSONObject) condition;
+                if (masterNodeId.equals(con.getString("nodeId"))) {
+                    return con.getString("name");
+                }
+            }
+        }
+        throw new BusinessException(ErrorMessage.CANNOT_FIND_MASTER);
+    }
+
+    @Override
+    public Map<String, String> burstList(String clusterId, String namespace, String middlewareName, String mode) {
+        Map<String, String> burstMap = new HashMap<>();
+        MiddlewareCR cr = middlewareCRService.getCR(clusterId, namespace, MiddlewareTypeEnum.REDIS.getType(), middlewareName);
+        JSONObject status = JSONObject.parseObject(cr.getMetadata().getAnnotations().get("status"));
+        if (status == null) {
+            return null;
+        }
+        JSONArray conditions = status.getJSONArray("conditions");
+        if (CollectionUtil.isEmpty(conditions)) {
+            return null;
+        }
+
+        Map<String, JSONObject> conditionMap = new HashMap<>();
+        // 哨兵模式
+        if ("sentinel".equals(mode)) {
+            conditions.forEach(condition -> {
+                JSONObject con = (JSONObject) condition;
+                if ("master".equals(con.getString("type"))) {
+                    String name = con.getString("name");
+                    String burstName = name.substring(0, name.lastIndexOf("-"));
+                    conditionMap.put(burstName, con);
+                }
+            });
+            conditions.forEach(condition -> {
+                JSONObject con = (JSONObject) condition;
+                if ("slave".equals(con.getString("type"))) {
+                    String name = con.getString("name");
+                    String burstName = name.substring(0, name.lastIndexOf("-"));
+                    JSONObject master = conditionMap.get(burstName);
+                    burstMap.put(master.getString("name"), con.getString("name"));
+                }
+            });
+        } else {
+            conditions.forEach(condition -> {
+                JSONObject con = (JSONObject) condition;
+                conditionMap.put(con.getString("nodeId"), con);
+            });
+            conditions.forEach(condition -> {
+                JSONObject con = (JSONObject) condition;
+                if ("slave".equals(con.getString("type"))) {
+                    JSONObject masterNode = conditionMap.get(con.getString("masterNodeId"));
+                    burstMap.put(masterNode.getString("name"), con.getString("name"));
+                }
+            });
+        }
+        return burstMap;
+
+    }
 }
