8d7a58fbb62ff17c4a175b7fb5ecd05c5e713f06
wangpenglei <wangpenglei2001@163.com>
2023-01-16 14:23:28 +0800

    feat - 节点迁移数据结构优化

--- src/main/java/com/harmonycloud/zeus/integration/cluster/bean/MigrateInfo.java
+++ src/main/java/com/harmonycloud/zeus/integration/cluster/bean/MigrateInfo.java
@@ -1,5 +1,7 @@
 package com.harmonycloud.zeus.integration.cluster.bean;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -11,8 +13,14 @@ import java.util.Date;
  **/
 @Accessors(chain = true)
 @Data
+@ApiModel("记录迁移状态信息")
 public class MigrateInfo {
+    @ApiModelProperty("迁移状态")
     private String status;
+
+    @ApiModelProperty("迁移失败原因")
     private String reason;
+
+    @ApiModelProperty("迁移时间")
     private Date migrateTimestamp;
 }
