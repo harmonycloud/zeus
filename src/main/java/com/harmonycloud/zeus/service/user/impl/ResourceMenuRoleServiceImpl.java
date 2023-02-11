1bcdf1e8ef648da6c30725761b8ba92b1a79c024
xutianhong <736950061@qq.com>
2023-02-11 15:39:21 +0800

    fix - 修改运维人员初始化菜单列表

--- src/main/java/com/harmonycloud/zeus/service/user/impl/ResourceMenuRoleServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/user/impl/ResourceMenuRoleServiceImpl.java
@@ -25,7 +25,7 @@ public class ResourceMenuRoleServiceImpl implements ResourceMenuRoleService {
 
     @Value("${system.user.admin.role-id:1}")
     private String adminRoleId;
-    @Value("${system.user.role.resource-menu:5,8,9,10,12,14,15,16}")
+    @Value("${system.user.role.resource-menu:8,9,10,12,14,15,16}")
     private String opsIds;
 
     @Autowired
