91e9864fe3db64fcd5dee5ba5113e327dd73e4e7
xutianhong <736950061@qq.com>
2023-02-16 11:36:12 +0800

    fix - 服务告警内容关键词过滤逻辑修改

--- src/main/java/com/harmonycloud/zeus/service/middleware/impl/MiddlewareAlertsServiceImpl.java
+++ src/main/java/com/harmonycloud/zeus/service/middleware/impl/MiddlewareAlertsServiceImpl.java
@@ -95,19 +95,13 @@ public class MiddlewareAlertsServiceImpl implements MiddlewareAlertsService {
             }
         }
         if (StringUtils.isNotEmpty(keyword)) {
-            String alertID = keyword.replaceAll("GJ","");
-            if (isNumeric(alertID)) {
-                queryWrapper.and(wrapper ->
-                        wrapper.like("alert_id",Integer.parseInt(alertID)).or().like("alert_expr",keyword));
-            } else {
-                queryWrapper.and(wrapper ->
-                        wrapper.or().eq("alert",keyword).or().eq("symbol",keyword)
-                                .or().like("threshold",keyword)
-                                .or().eq("alert_time",keyword).or().eq("alert_times",keyword)
-                                .or().eq("content",keyword).or().eq("description",keyword)
-                                .or().like("alert_expr",keyword)
-                );
-            }
+            queryWrapper.and(wrapper ->
+                    wrapper.or().eq("alert",keyword).or().eq("symbol",keyword)
+                            .or().like("threshold",keyword)
+                            .or().eq("alert_time",keyword).or().eq("alert_times",keyword)
+                            .or().eq("description",keyword)
+                            .or().like("alert_expr",keyword)
+            );
         }
         List<AlertRuleId> alertInfos = alertRuleIdMapper.selectList(queryWrapper);
         PageInfo<MiddlewareAlertsDTO> alertsDTOPageInfo = new PageInfo<>();
