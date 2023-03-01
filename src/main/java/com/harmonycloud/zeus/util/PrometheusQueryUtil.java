d1f1de9b88a9569abafef8526665bdce5367566b
xutianhong <736950061@qq.com>
2023-03-01 12:02:29 +0800

    fix - 中间件存储监控查询语句拼接逻辑修改

--- src/main/java/com/harmonycloud/zeus/util/PrometheusQueryUtil.java
+++ src/main/java/com/harmonycloud/zeus/util/PrometheusQueryUtil.java
@@ -12,14 +12,14 @@ public class PrometheusQueryUtil {
 
     public static String queryHitachiPodTotal(String serialId, String poolId, String namespace, String pvc) {
         return String.format(
-            "sum(spc_volume_total_capacity{serial_id=\"%s\",pool_id=\"%s\",namespace=\"%s\",persistentvolumeclaim=~\"%s\"}) by (persistentvolumeclaim) /1024/1024/1024 ",
-            serialId, poolId, namespace, pvc);
+            "sum(spc_volume_total_capacity{namespace=\"%s\",persistentvolumeclaim=~\"%s\"}) by (persistentvolumeclaim) /1024/1024/1024 ",
+            namespace, pvc);
     }
 
     public static String queryHitachiPodUsed(String serialId, String poolId, String namespace, String pvc) {
         return String.format(
-            "sum(spc_volume_used_capacity{serial_id=\"%s\",pool_id=\"%s\",namespace=\"%s\",persistentvolumeclaim=~\"%s\"}) by (persistentvolumeclaim) /1024/1024/1024",
-            serialId, poolId, namespace, pvc);
+            "sum(spc_volume_used_capacity{namespace=\"%s\",persistentvolumeclaim=~\"%s\"}) by (persistentvolumeclaim) /1024/1024/1024",
+            namespace, pvc);
     }
 
 }
