5dd53ca6e69573ed414992e849981731649ce682
liyinlong <liyinlong>
2023-03-20 14:18:23 +0800

    fix-哨兵模式redis返回了代理节点资源信息

--- src/main/java/com/middleware/zeus/operator/impl/RedisOperatorImpl.java
+++ src/main/java/com/middleware/zeus/operator/impl/RedisOperatorImpl.java
@@ -235,8 +235,7 @@ public class RedisOperatorImpl extends AbstractRedisOperator implements RedisOpe
             convertResourcesByHelmChart(middleware, middleware.getType(), redisQuota.getJSONObject(RESOURCES));
             middleware.getQuota().get(middleware.getType()).setNum(redisQuota.getInteger(REPLICAS));
             // 读写分离
-            if (values.containsKey("predixy") && values.getJSONObject("predixy").getBoolean("enableProxy") != null
-                    && values.getJSONObject("predixy").getBoolean("enableProxy")) {
+            if (values.containsKey("predixy")) {
                 JSONObject predixy = values.getJSONObject("predixy");
                 ReadWriteProxy readWriteProxy = new ReadWriteProxy();
                 readWriteProxy.setEnabled(predixy.getBoolean("enableProxy"));
