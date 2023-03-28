20a6bbc75f0c977a5d70c789f38d22feb68720af
wangpenglei <wangpengleo2001@163.com>
2023-03-28 11:01:08 +0800

    feat - 部署文件兼容平台灾备

--- deploy/install.sh
+++ deploy/install.sh
@@ -5,6 +5,7 @@ LINE_TYPE=$2
 IMAGE_REPO=$3
 STORAGE_CLASS=$4
 HA=$5
+DR=$6
 
 if [ $LINE_TYPE == "offline" ]; then
   echo "######  Push images  ######"
@@ -34,7 +35,14 @@ function deploy_helm() {
   if [ $HA == "true" ]; then
     MYSQL_REPLICATE="replicaCount=2"
   fi
-  helm install -n zeus zeus-mysql deploy/mysql-operator --set mysql-operator.enabled=false,image.repository=$IMAGE_REPO"/middleware",args.root_password="ZeuS@Middleware01",storageClassName=$STORAGE_CLASS,storageSize=10Gi,$MYSQL_REPLICATE
+
+  if [ $DR == "false" ]; then
+    helm install -n zeus zeus-mysql deploy/mysql-operator --set mysql-operator.enabled=false,image.repository=$IMAGE_REPO"/middleware",args.root_password="ZeuS@Middleware01",storageClassName=$STORAGE_CLASS,storageSize=10Gi,$MYSQL_REPLICATE
+  fi
+  if [ $DR == "true" ]; then
+    helm install -n zeus zeus-mysql -f deploy/mysql-operator/values-dr.yaml deploy/mysql-operator --set mysql-operator.enabled=false,image.repository=$IMAGE_REPO"/middleware",args.root_password="ZeuS@Middleware01",storageClassName=$STORAGE_CLASS,storageSize=10Gi,$MYSQL_REPLICATE
+  fi
+
   # install zeus platform
   HELM_ARGS="global.replicaCount=1"
   if [ $HA == "true" ]; then
