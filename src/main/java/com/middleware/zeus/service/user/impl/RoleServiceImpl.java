97f2dcfb4b2f689636f25c571a74482c830b7d31
xutianhong <736950061@qq.com>
2023-03-31 15:50:09 +0800

    fix - 角色列表排序

--- src/main/java/com/middleware/zeus/service/user/impl/RoleServiceImpl.java
+++ src/main/java/com/middleware/zeus/service/user/impl/RoleServiceImpl.java
@@ -136,7 +136,8 @@ public class RoleServiceImpl implements RoleService {
             }
             return roleDto;
         }).sorted((o1, o2) -> o1.getWeight() == null && o2.getWeight() == null ? 0
-            : o1.getWeight() == null ? 1 : o2.getWeight() == null ? -1 : 0).filter(roleDto -> {
+            : o1.getWeight() == null ? 1 : o2.getWeight() == null ? 1 : o1.getWeight().compareTo(o2.getWeight()))
+            .filter(roleDto -> {
                 if (StringUtils.isNotEmpty(key)) {
                     return roleDto.getName().contains(key) || roleDto.getDescription().contains(key);
                 }
