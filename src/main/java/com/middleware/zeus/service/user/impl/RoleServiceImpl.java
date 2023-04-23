f33dbd7b83e83099410ee096dae9517d3ac0e452
xutianhong <736950061@qq.com>
2023-04-23 17:05:18 +0800

    fix - 用户菜单查询 listMenuByRoleId方法逻辑修改

--- src/main/java/com/middleware/zeus/service/user/impl/RoleServiceImpl.java
+++ src/main/java/com/middleware/zeus/service/user/impl/RoleServiceImpl.java
@@ -165,14 +165,14 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public List<ResourceMenuDto> listMenuByRoleId(UserDto userDto, String organId, String projectId) {
         Set<BeanResourceMenuRole> set;
-        if (userDto.getIsAdmin() || (StringUtils.isEmpty(organId) && StringUtils.isEmpty(projectId))) {
+        if (userDto.getIsAdmin() || (StringUtils.isEmpty(organId) && StringUtils.isEmpty(projectId) && userDto.getManager() == null)) {
             set = new HashSet<>(resourceMenuRoleService.listAdminMenu());
         } else {
             set = new HashSet<>();
             // 处理管理类型的角色的菜单列表
             List<UserRole> managerUserRoleList = userDto.getUserRoleList().stream().filter(userRole -> StringUtils.isEmpty(userRole.getOrganId()) && StringUtils.isEmpty(userRole.getProjectId())).collect(Collectors.toList());
             if (!CollectionUtils.isEmpty(managerUserRoleList)){
-                set.addAll(resourceMenuRoleService.list(String.valueOf(managerUserRoleList.get(0).getRoleId())));
+
             }
             // 获取组织管理员menu
             if (StringUtils.isNotEmpty(organId)) {
