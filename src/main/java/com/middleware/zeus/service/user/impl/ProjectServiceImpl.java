b0e6df5e7d0c26a0d8c9a1fea2abf26581a1f32b
liyinlong <liyinlong>
2023-04-26 13:39:07 +0800

    fix-添加日志

--- src/main/java/com/middleware/zeus/service/user/impl/ProjectServiceImpl.java
+++ src/main/java/com/middleware/zeus/service/user/impl/ProjectServiceImpl.java
@@ -706,6 +706,7 @@ public class ProjectServiceImpl extends AbstractProjectService implements Projec
      */
     public List<Namespace> addOtherInfo(List<Namespace> namespaces, String clusterId) {
         try {
+            log.info("添加分区其他信息");
             List<Namespace> nsList = namespaceService.list(clusterId, false, null);
             Map<String, Namespace> nsMap = new HashMap<>();
             nsList.forEach(ns -> {
