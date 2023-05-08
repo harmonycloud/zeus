67031966b76015f6130009af2bc052ef57a339c9
liyinlong <liyinlong>
2023-05-08 16:32:18 +0800

    fix-备份记录名称错误

--- src/main/java/com/middleware/zeus/controller/middleware/MiddlewareBackupController.java
+++ src/main/java/com/middleware/zeus/controller/middleware/MiddlewareBackupController.java
@@ -105,7 +105,7 @@ public class MiddlewareBackupController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "clusterId", value = "集群id", paramType = "path", dataTypeClass = String.class),
             @ApiImplicitParam(name = "namespace", value = "命名空间", paramType = "path", dataTypeClass = String.class),
-            @ApiImplicitParam(name = "backupName", value = "备份任务名称", paramType = "query", dataTypeClass = String.class),
+            @ApiImplicitParam(name = "backupId", value = "备份任务id", paramType = "query", dataTypeClass = String.class),
             @ApiImplicitParam(name = "backupMode", value = "任务类型（single：单次备份，period：周期备份）", paramType = "query", dataTypeClass = String.class)
     })
     @GetMapping("/detail")
