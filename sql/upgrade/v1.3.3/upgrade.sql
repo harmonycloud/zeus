24c04f16988af5e2f998d6f45187c14b96f57354
xutianhong <736950061@qq.com>
2023-05-15 15:20:45 +0800

    fix - upgrade sql执行顺序修改

--- sql/upgrade/v1.3.3/upgrade.sql
+++ sql/upgrade/v1.3.3/upgrade.sql
@@ -1,10 +1,11 @@
 -- 20230419 xutianhong
 -- 角色列表添加类型字段
 alter table role add type varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '角色类型' after name;
+update `role` set type='normal';
 update `role` set type='manager' where weight='1';
 update `role` set type='manager' where weight='2';
 update `role` set type='manager' where weight='3';
-update `role` set type='normal';
+
 
 -- 20230424 wangpenglei
 -- 参数相关新增role字段
