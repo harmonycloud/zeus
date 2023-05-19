82360165a789e59808850fdc6447ea97071bc09c
xutianhong <736950061@qq.com>
2023-05-19 13:59:21 +0800

    update - minio组件更新

--- src/main/java/com/middleware/zeus/service/components/impl/MinioServiceImpl.java
+++ src/main/java/com/middleware/zeus/service/components/impl/MinioServiceImpl.java
@@ -60,7 +60,7 @@ public class MinioServiceImpl extends AbstractBaseOperator implements MinioServi
     @Override
     protected void install(String setValues, MiddlewareClusterDTO cluster) {
         helmChartService.installComponents(ComponentsEnum.MINIO.getName(), "minio", setValues,
-                componentsPath + File.separator + "minio/charts/minio", cluster);
+                componentsPath + File.separator + "minio", cluster);
     }
 
     @Override
