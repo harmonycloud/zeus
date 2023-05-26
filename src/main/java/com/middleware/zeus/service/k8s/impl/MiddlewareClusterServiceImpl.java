da1638eb61bd46f420e4a512fd3929775ad4c8e1
liyinlong <liyinlong>
2023-05-26 16:20:05 +0800

    fix-集群详情报错

--- src/main/java/com/middleware/zeus/service/k8s/impl/MiddlewareClusterServiceImpl.java
+++ src/main/java/com/middleware/zeus/service/k8s/impl/MiddlewareClusterServiceImpl.java
@@ -68,7 +68,7 @@ public class MiddlewareClusterServiceImpl implements MiddlewareClusterService {
                         ? DateUtils.parseUTCDate(c.getMetadata().getCreationTimestamp()) : c.getMetadata().getCreationTimestamp());
                 cluster.setAttributes(attributes);
             }
-            if (StringUtils.isEmpty(info.getAccessToken())) {
+            if (StringUtils.isNotEmpty(info.getAccessToken())) {
                 cluster.setAccessToken(info.getAccessToken());
             }
             return SerializationUtils.clone(cluster);
