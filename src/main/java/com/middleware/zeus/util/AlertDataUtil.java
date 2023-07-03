5395ebda028f3538a5feba82ed0521d8f50aa0e7
xutianhong <736950061@qq.com>
2023-07-03 10:11:27 +0800

    fix - 数据总览告警信息查询线形图时区修改为东8区

--- src/main/java/com/middleware/zeus/util/AlertDataUtil.java
+++ src/main/java/com/middleware/zeus/util/AlertDataUtil.java
@@ -32,16 +32,17 @@ public class AlertDataUtil {
             });
         }
 
+        // todo 暂时写死东8区
         hourList.forEach(item -> {
             Map<String, Object> single = new HashMap();
             if (map.containsKey(item)) {
-                single.put(DateUtil.addHourAndGetNormal(item, 0), map.get(item));
+                single.put(DateUtil.addHourAndGetNormal(item, 8), map.get(item));
                 single.put("num", map.get(item));
-                single.put("alerttime", DateUtil.addHourAndGetNormal(item, 0));
+                single.put("alerttime", DateUtil.addHourAndGetNormal(item, 8));
             } else {
-                single.put(DateUtil.addHourAndGetNormal(item, 0), "0");
+                single.put(DateUtil.addHourAndGetNormal(item, 8), "0");
                 single.put("num", 0);
-                single.put("alerttime", DateUtil.addHourAndGetNormal(item, 0));
+                single.put("alerttime", DateUtil.addHourAndGetNormal(item, 8));
             }
             resList.add(single);
         });
