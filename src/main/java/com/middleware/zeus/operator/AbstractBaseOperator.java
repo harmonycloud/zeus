f807fd4464f5a930ce1db5246a1e609b09c554ee
xutianhong <736950061@qq.com>
2024-01-22 10:40:25 +0800

    fix - 移除中间件发布时的默认开启postgresql审计日志生成

--- src/main/java/com/middleware/zeus/operator/AbstractBaseOperator.java
+++ src/main/java/com/middleware/zeus/operator/AbstractBaseOperator.java
@@ -927,15 +927,6 @@ public abstract class AbstractBaseOperator {
         if (middleware.getAudit() != null){
             JSONObject features = values.getJSONObject("features");
             checkAndSetAuditSqlStatus(features, middleware);
-            // 特殊处理postgresql
-            if (middleware.getType().equals(MiddlewareTypeEnum.POSTGRESQL.getType())){
-                JSONObject args = values.getJSONObject("args");
-                if (args == null){
-                    args = new JSONObject();
-                }
-                args.put("pgaudit.log", "WRITE,DDL");
-                values.put("args", args);
-            }
         }
     }
 
