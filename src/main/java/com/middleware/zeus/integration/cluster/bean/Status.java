c8d684a628ea884eb6590141e8e4197158c72e10
wangpenglei <wangpengleo2001@163.com>
2024-11-28 16:04:37 +0800

    fix - 兼容不同中间件status.condition.status类型不同

--- src/main/java/com/middleware/zeus/integration/cluster/bean/Status.java
+++ src/main/java/com/middleware/zeus/integration/cluster/bean/Status.java
@@ -30,7 +30,7 @@ public class Status {
         private String name;
         private String nodeName;
         private String podIP;
-        private String status;
+        private Object status;
         private String type;
     }
 
