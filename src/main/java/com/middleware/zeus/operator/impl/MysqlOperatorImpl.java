2a13f217214aa84c7f3e945c35f2716eddba503b
xutianhong <736950061@qq.com>
2024-11-29 15:28:07 +0800

    feat - mysql部署慢日志参数使用默认值

--- src/main/java/com/middleware/zeus/operator/impl/MysqlOperatorImpl.java
+++ src/main/java/com/middleware/zeus/operator/impl/MysqlOperatorImpl.java
@@ -168,12 +168,6 @@ public class MysqlOperatorImpl extends AbstractMysqlOperator implements MysqlOpe
                 values.put(MysqlConstant.SPEC_TYPE, mysqlDTO.getType());
             }
         }
-        // 配置开启/关闭 审计日志和慢日志
-        if (middleware.getSlowSql() != null && middleware.getSlowSql()) {
-            mysqlArgs.put(SLOW_QUERY_LOG, ON);
-        } else {
-            mysqlArgs.put(SLOW_QUERY_LOG, OFF);
-        }
 
         //配置mysql环境变量
         if (!CollectionUtils.isEmpty(middleware.getEnvironment())) {
