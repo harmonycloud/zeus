0382de03ae3132c8d770137a9c65157b2f609f58
xutianhong <736950061@qq.com>
2024-12-09 14:12:13 +0800

    fix - postgresql查询手动切换状态exec方法修改

--- src/main/java/com/middleware/zeus/operator/impl/PostgresqlOperatorImpl.java
+++ src/main/java/com/middleware/zeus/operator/impl/PostgresqlOperatorImpl.java
@@ -295,7 +295,7 @@ public class PostgresqlOperatorImpl extends AbstractPostgresqlOperator implement
                 middleware.getNamespace(), cluster.getAddress(), cluster.getAccessToken(), patroniName, patroniPort);
         List<String> resList = new ArrayList<>();
         try {
-            resList = CmdExecUtil.runCmd(execCommand);
+            resList = CmdExecUtil.execCmd(execCommand, null);
         } catch (Exception e) {
             log.error("查询自动切换失败", e);
         }
