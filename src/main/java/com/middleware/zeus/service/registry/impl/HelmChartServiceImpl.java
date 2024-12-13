8a0614f25ae8d73171872a0d7fc178cb462f869e
xutianhong <736950061@qq.com>
2024-12-13 14:00:00 +0800

    fix - 还原错误修改

--- src/main/java/com/middleware/zeus/service/registry/impl/HelmChartServiceImpl.java
+++ src/main/java/com/middleware/zeus/service/registry/impl/HelmChartServiceImpl.java
@@ -511,14 +511,6 @@ public class HelmChartServiceImpl extends AbstractRegistryService implements Hel
         String chartName = middleware.getChartName();
         String chartVersion = middleware.getChartVersion();
 
-        String cmd = String.format("helm upgrade %s %s --values %s --set %s -n %s --kube-apiserver %s --kubeconfig %s ",
-                middleware.getName(), "tgzFilePath", "tempValuesYamlPath", updateValues, middleware.getNamespace(),
-                cluster.getAddress(), clusterCertService.getKubeConfigFilePath(cluster.getId()));
-
-        if (StringUtils.isNotBlank(updateStringValues)) {
-            cmd = cmd + " --set-string " + updateStringValues + " ";
-        }
-
         // 先获取chart文件
         HelmChartFile helmChart = getHelmChartFromMysql(chartName, chartVersion);
 
@@ -541,7 +533,7 @@ public class HelmChartServiceImpl extends AbstractRegistryService implements Hel
             tgzFilePath = packageChart(helmChart.getTarFileName(), chartName, chartVersion);
         }
         String tempValuesYamlPath = tempValuesYamlDir + File.separator + tempValuesYamlName;
-        cmd = String.format("helm upgrade %s %s --values %s --set %s -n %s --kube-apiserver %s --kubeconfig %s ",
+        String cmd = String.format("helm upgrade %s %s --values %s --set %s -n %s --kube-apiserver %s --kubeconfig %s ",
             middleware.getName(), tgzFilePath, tempValuesYamlPath, updateValues, middleware.getNamespace(),
             cluster.getAddress(), clusterCertService.getKubeConfigFilePath(cluster.getId()));
 
