87cc051eac038884234b62c39b8974bfa34026f4
xutianhong <736950061@qq.com>
2024-12-20 14:12:58 +0800

    fix - middleware api请求失败onError添加异常处理

--- src/main/java/com/middleware/zeus/interceptor/MiddlewareApiInterceptor.java
+++ src/main/java/com/middleware/zeus/interceptor/MiddlewareApiInterceptor.java
@@ -51,13 +51,18 @@ public class MiddlewareApiInterceptor implements Interceptor {
     @Override
     public void onError(ForestRuntimeException ex, ForestRequest req, ForestResponse res) {
         log.error(res.getContent());
-        if (StringUtils.isNotEmpty(res.getContent())) {
-            JSONObject jsonObject = JSONObject.parseObject(res.getContent());
-            String err = jsonObject.getString("err");
-            throw new BusinessException(ErrorMessage.MIDDLEWARE_API_REQUEST_ERROR, err);
-        } else {
+        try {
+            if (StringUtils.isNotEmpty(res.getContent())) {
+                JSONObject jsonObject = JSONObject.parseObject(res.getContent());
+                String err = jsonObject.getString("err");
+                throw new BusinessException(ErrorMessage.MIDDLEWARE_API_REQUEST_ERROR, err);
+            } else {
+                throw new BusinessException(ErrorMessage.MIDDLEWARE_API_REQUEST_ERROR, ex.getMessage());
+            }
+        } catch (Exception e){
             throw new BusinessException(ErrorMessage.MIDDLEWARE_API_REQUEST_ERROR, ex.getMessage());
         }
+
     }
 
 }
