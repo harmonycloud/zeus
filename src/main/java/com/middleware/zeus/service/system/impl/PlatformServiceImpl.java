ebcd02ca9f37020ce645596359336cc79f55548f
xutianhong <736950061@qq.com>
2025-03-24 14:27:14 +0800

    fix - 中间件查询详情时添加mongodb类型判断

--- src/main/java/com/middleware/zeus/service/system/impl/PlatformServiceImpl.java
+++ src/main/java/com/middleware/zeus/service/system/impl/PlatformServiceImpl.java
@@ -57,12 +57,12 @@ public class PlatformServiceImpl implements PlatformService {
     @PostConstruct
     public void init(){
         try {
-            JSONObject values = helmChartService.getZeusMysqlInstallValues();
-            boolean isSlave = "master-slave".equals(values.getString("type"));
-            if (!isSlave && values.getJSONObject("args") != null && values.getJSONObject("args").containsKey("disasterRecoverySwitched")
-                    && values.getJSONObject("args").getBoolean("disasterRecoverySwitched")){
-                AVAILABLE.put("available", false);
-            }
+//            JSONObject values = helmChartService.getZeusMysqlInstallValues();
+//            boolean isSlave = "master-slave".equals(values.getString("type"));
+//            if (!isSlave && values.getJSONObject("args") != null && values.getJSONObject("args").containsKey("disasterRecoverySwitched")
+//                    && values.getJSONObject("args").getBoolean("disasterRecoverySwitched")){
+//                AVAILABLE.put("available", false);
+//            }
         } catch (Exception e){
             log.error("初始化平台灾备可访问失败");
         }
