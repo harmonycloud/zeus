d4c3f1d3b75cb2f1b647c4a81f2223ec60cf8b06
xutianhong <736950061@qq.com>
2025-07-10 14:11:15 +0800

    fix - 修复告警邮件标题中字符串格式化占位符错误

--- src/main/java/com/middleware/zeus/service/user/impl/MailServiceImpl.java
+++ src/main/java/com/middleware/zeus/service/user/impl/MailServiceImpl.java
@@ -234,7 +234,7 @@ public class MailServiceImpl implements MailService {
                 String.format("【%s】Zeus %s Alarm - %s - %s Name: %s", level, target, alertName, target, middlewareName);
         } else {
             String target = alertType.equals(SERVICE) ? "中间件" : (alertType.equals(SYSTEM) ? "系统" : "集群");
-            subject = String.format("【%s】Zeus%s告警-%s-$s名称: $s", level, target, alertName,
+            subject = String.format("【%s】Zeus%s告警-%s-%s名称: %s", level, target, alertName,
                 target.equals("中间件") ? "服务" : target, middlewareName);
         }
         return subject;
